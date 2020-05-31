48b1a3d48f0938783d637e3fe4120aee72b22eee
yeojhenrie <65714639+yeojhenrie@users.noreply.github.com>
2020-05-31 09:29:04 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+/ Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('message', msg => {
+  if (msg.content.includes("fuck")) {
+    msg.reply('Fuck You!');
+  } else if (msg.content.includes("you just advanced to level")) {
+    msg.reply('Fuck You!');
+  }
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
+});
